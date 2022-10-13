c2d853209d769b59d3ea4f6acad3d9ba65c695cc
Sebastian Domsch <Sebastian.Domsch@googlemail.com>
2022-10-13 19:10:29 +0200

    add artificial swarm intelligence to streamlit app

--- optimize.py
+++ optimize.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import os
 from pathlib import Path
+import numpy as np
 import pandas as pd
 import plotly.express as px
+from pyswarm import pso
 from utils import normalized_returns, weight_creator, portfolio_returns, portfolio_std, portfolio_sharp_ratio, sim2_df, min_variance_portfolio
 
 
@@ -24,10 +26,10 @@ def run_optimize_app():
     ########################################
     investment_sum = st.slider(label='select investment sum [€]', min_value=0, max_value=10000, value=100, step=100)
     risk_free_rate = st.slider(label='select risk free interest rate in percent', min_value=-2.0, max_value=10.0, value=3.0, step=0.5)
-    n_experiments = st.slider(label='select number of experiments', min_value=1000, max_value=10000, value=1000, step=1000)
+    n_experiments = st.slider(label='select number of Monte-Carlo simulationss', min_value=1000, max_value=10000, value=1000, step=1000)
     
-    if st.button('Run Monte-Carlo Experiment'):
-        sim_state = st.text('running experiment ...')
+    if st.button('Run Monte-Carlo Simulation'):
+        sim_state = st.text('running simulations ...')
         w = []
         returns = []
         stds = []
@@ -51,7 +53,7 @@ def run_optimize_app():
         df_simulation = sim2_df(returns, stds, srs, w, stock_names)
         df_simulation.to_csv(DATAPATH / 'simulation.csv') # save dataset to local folder
         
-        sim_state = st.text('running experiment ...done!')
+        sim_state = st.text('running simulations ...done!')
 
 
     #############################################
@@ -107,4 +109,89 @@ def run_optimize_app():
                 title="Optimal Investment Strategy for Given Risk Investing " + str(investment_sum) + '€.').update_layout(legend={'xanchor':'right', 'yanchor':'bottom'}
                 )
     st.plotly_chart(fig)
+
+
+    ########################################
+    # run particle swarm optimization
+    ########################################
+    if st.button('Run Artificial Swarm Intelligence for further Optimization'):
+        sim_state = st.text('running artificial swarm intelligence ...')
+
+        # import data
+        df = pd.read_csv(DATAPATH / 'data.csv', index_col='Date')
+        df.index = pd.to_datetime(df.index)
+        # get stock names
+        stock_names = list(df.columns)
+        # make returns df
+        df_returns = normalized_returns(df)
+
+        ########################
+        # particle swarm algo
+        ########################
+        # define variable bounds
+        lbs = np.repeat(0.0, len(stock_names))
+        ubs = np.repeat(1.0, len(stock_names))
+        # initialize weights
+        x0 = weight_creator(df_returns)
+        # define objective function
+        def f(x):
+            # normalize weights
+            x = x/np.sum(x)
+            #calc portfolio´s sharp ratio
+            portfolio_return = portfolio_returns(df_returns, x)
+            portfolio_stdev = portfolio_std(df_returns, x) 
+            portfolio_sr = portfolio_sharp_ratio(portfolio_return, portfolio_stdev, risk_free_rate/100)
+            # add a penalty for risks higher than risk_limit
+            if portfolio_stdev > selected_maximal_risk:  
+                penalty = 10000*(portfolio_stdev - selected_maximal_risk)**2
+            elif portfolio_stdev < 0.9*selected_maximal_risk:  
+                penalty = 10000*(portfolio_stdev - selected_maximal_risk)**2
+            else:
+                penalty = 0    
+            return -portfolio_sr + penalty
+        # def constraint function 
+        def cons(x):
+            return [] # in this case, all the contraints are incorporated in the objective function f
+        # search best weights
+        weights_opt, fopt = pso(f, lbs, ubs, x0, cons)
+        sim_state = st.text('running artificial swarm intelligence ...done!')
+
+        # normalize weights
+        weights_opt = weights_opt / np.sum(weights_opt)
+        # calculate sharp ratio
+        portfolio_return = portfolio_returns(df_returns, weights_opt)
+        portfolio_stdev = portfolio_std(df_returns, weights_opt) 
+        portfolio_sr = portfolio_sharp_ratio(portfolio_return, portfolio_stdev, risk_free_rate/100)
+
+        # plot returns vs risk
+        fig = px.scatter(x=df_simulation['portfolio standard dev'], 
+                        y=df_simulation['portfolio return']*100,
+                        color=df_simulation['portfolio sharp ratio']*100,
+                        labels={'y': 'return [%]', 'x': 'standard deviation', 'color': 'sharp ratio'},
+                        width=600,
+                        title='Portfolio´s Returns and Risks Monte-Carlo Simulation')
+
+        fig.add_scatter(x=np.array(portfolio_stdev), 
+                        y=np.array(portfolio_return*100),
+                        marker=dict(color='blue', size=15))
+        fig.layout.showlegend = False                
+
+        fig.add_hline(y=portfolio_return*100, 
+                    line_width=3, line_dash="dash", line_color="green",
+                    annotation_text="optimized return using swarm intelligence: sharp ratio={:.2f}".format(portfolio_sr*100), 
+                    annotation_position="top right")
+        fig.add_vline(x=portfolio_stdev, line_width=3, line_dash="dash", line_color="green")
+        st.plotly_chart(fig)
+
+        # plotting optimal weights
+        df_opt = pd.DataFrame(data=weights_opt*investment_sum, index=stock_names, columns=['Investment [€]']).T
+        
+        fig = px.bar(df_opt.T.sort_values('Investment [€]'),
+                orientation='h',
+                #height=800,
+                labels={"index": "stock", 'value': 'stock investment [€]'},
+                title="Optimal Investment Strategy for Given Risk Investing " + str(investment_sum) + '€.').update_layout(legend={'xanchor':'right', 'yanchor':'bottom'}
+                )
+        st.plotly_chart(fig)
+        
 
